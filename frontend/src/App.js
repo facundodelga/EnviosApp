d471494abdd378f98a344145b42e4ad01e050266
facundodelga <128828081+facundodelga@users.noreply.github.com>
2025-03-15 15:19:25 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import LoginContainer from './components/LoginContainer';
 
+
 function App() {
   return (
+    
     <div className="App">
+      
       <LoginContainer />
     </div>
   );
